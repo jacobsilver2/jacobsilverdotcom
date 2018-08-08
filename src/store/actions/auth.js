e5e8c7af2c0cd09d21c25bde082a586ca796c926
jacobsilver2 <jacobsilver2@mac.com>
2018-08-08 14:19:27 -0400

    changed environment variable structure

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -50,9 +50,9 @@ export const auth = (email, password, isSignUp) => {
       password: password,
       returnSecureToken: true,
     }
-    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCFLMgQYaC_jPEnZsLvc4qqVQUev1KoTKE'
+    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + process.env.REACT_APP_FIREBASE_API
     if (!isSignUp) {
-      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCFLMgQYaC_jPEnZsLvc4qqVQUev1KoTKE'
+      url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + process.env.REACT_APP_FIREBASE_API
     }
     axios.post(url, authData)
       .then(resp => {
