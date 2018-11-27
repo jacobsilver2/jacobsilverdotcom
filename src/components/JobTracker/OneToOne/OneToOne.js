68064117d41e27ae77cab5f837731ab1265ffe14
Jacob Silver <jacobsilver2@mac.com>
2018-11-27 11:29:19 -0500

    added files and directories for job tracker

---
+++ src/components/JobTracker/OneToOne/OneToOne.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Form from "../../UI/Input/Input";
+import classes from './Input.css';
+
+class OneToOne extends React {
+
+   render() {
+     return (
+     
+     ) 
+   }
+}
